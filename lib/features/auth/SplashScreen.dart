415ed8e6c52ea55aaa5918de189880b72cc0050c
dodzonduty <mohamed.Ibrahem202011@gmail.com>
2025-02-28 21:42:54 +0200

    enhancements

--- lib/features/auth/SplashScreen.dart
+++ lib/features/auth/SplashScreen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
