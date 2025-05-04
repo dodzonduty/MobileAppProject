e8de83a63ed0cf49a5806d84eefb3ba4f4785ebf
hager_salah <salahhager550@gmail.com>
2025-05-04 21:55:01 +0300

    notifications_setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,33 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
+import 'package:project/features/profile/Profile.dart';
+import 'features/auth/SplashScreen.dart';
 import 'features/auth/login_page.dart';
 import 'features/auth/register_page.dart';
-import 'features/profile/Profile.dart';
-import 'features/home/view/home_page.dart' as home1;
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'package:project/features/home/view/home_page.dart' as home1;
 import 'features/events/view/root_screen.dart';
-import 'features/transport/transport.dart';
 import 'BottomNavigetion.dart';
-import 'features/database/Database.dart'; // Updated import name
+import 'features/transport/transport.dart';
+import 'features/notifications/Notifications.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  await DatabaseHelper().db; // ✅ Ensure DB is created
+  final notificationService = NotificationService();
+  await notificationService.init();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginPage(), // You can also use SplashScreen() here
+      home: LoginScreen(),
       routes: {
         '/login': (context) => LoginPage(),
         '/register': (context) => RegisterPage(),
